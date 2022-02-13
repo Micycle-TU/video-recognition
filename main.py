55ebfcf077af0011256b7ede8738b0464c3fa66f
Micycle-TU <tudehao6@163.com>
2022-02-13 01:06:10 +0100

    Update main file

--- main.py
+++ main.py
@@ -1,41 +1,71 @@
 import numpy as np
 import torch
 from torch import nn
-from video_dataset import  VideoFrameDataset, ImglistToTensor
+from video_dataset import VideoFrameDataset, ImglistToTensor
 from torchvision import transforms
 import torch
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import ImageGrid
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.axes_grid1 import ImageGrid
 import os
 import pytorchvideo.models.vision_transformers
 import torch.nn.functional as F
 import torch.optim as optim
 
+
+batch = 6
+num_seg = 6
+frames_per_seg = 1
+resize = 224
+temporal_frame = num_seg * frames_per_seg
+learning_rate = 0.000025
+weight_decay = 0.1
+classes = 50
+
+
+embed_dim_mul = [[1, 2.0], [3, 2.0], [14, 2.0]]
+atten_head_mul = [[1, 2.0], [3, 2.0], [14, 2.0]]
+pool_q_stride_size = [[1, 1, 2, 2], [3, 1, 2, 2], [14, 1, 2, 2]]
+pool_kv_stride_adaptive = [1, 8, 8]
+pool_kvq_kernel = [3, 3, 3]
 def make_kinetics_mvit():
   return pytorchvideo.models.vision_transformers.create_multiscale_vision_transformers(
-      spatial_size=128,
-      temporal_size=6,# RGB input from Kinetics
+      spatial_size=resize,
+      temporal_size=temporal_frame,# RGB input from Kinetics
       depth=16, # For the tutorial let's just use a 50 layer network
-      head_num_classes=50, # Kinetics has 400 classes so we need out final head to align
+      embed_dim_mul=embed_dim_mul,
+      atten_head_mul=atten_head_mul,
+      pool_q_stride_size=pool_q_stride_size,
+      pool_kv_stride_adaptive=pool_kv_stride_adaptive,
+      pool_kvq_kernel=pool_kvq_kernel,
+      head_num_classes=classes, # Kinetics has 400 classes so we need out final head to align
   )
 
-videos_root = os.path.join('D:\\', 'UCF50_annotation')
-#videos_root = os.path.join('D:\\', 'demo_dataset')
+def make_kinetics_resnet():
+  return pytorchvideo.models.resnet.create_resnet(
+      input_channel=3,
+      model_depth=50,
+      model_num_class=50,
+      norm=nn.BatchNorm3d,
+      activation=nn.ReLU,
+  )
+
+videos_root = os.path.join('/media/ysun1/Seagate_1/Dehao/data', 'UCF50_annotation')
+#videos_root = os.path.join('/media/ysun1/Seagate_1/Dehao/data', 'Sub_ucf50')
 annotation_file = os.path.join(videos_root, 'annotations.txt')
 
 
 preprocess = transforms.Compose([
         ImglistToTensor(),# list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
-        transforms.Resize(128),  # image batch, resize smaller edge to 299
-        transforms.CenterCrop(128),
+        transforms.Resize(resize),  # image batch, resize smaller edge to 299
+        transforms.CenterCrop(resize),
         # image batch, center crop to square 299x299
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.455], std=[0.229, 0.224, 0.225]),
     ])
 
 dataset = VideoFrameDataset(
         root_path=videos_root,
         annotationfile_path=annotation_file,
-        num_segments=6,
+        num_segments=num_seg,
         frames_per_segment=1,
         imagefile_template='img_{:05d}.jpg',
         transform=preprocess,
@@ -50,19 +80,20 @@ print('Video Tensor Size:', frame_tensor.size())
 
 dataloader = torch.utils.data.DataLoader(
         dataset=dataset,
-        batch_size=2,
+        batch_size=batch,
         shuffle=True,
-        num_workers=0,
+        num_workers=4,
         pin_memory=True
     )
 
-videos_root_val = os.path.join('D:\\', 'UCF50_val_annotation')
+videos_root_val = os.path.join('/media/ysun1/Seagate_1/Dehao/data', 'UCF50_val_annotation')
+#videos_root_val = os.path.join('/media/ysun1/Seagate_1/Dehao/data', 'Sub_val_ucf50')
 annotation_file_val = os.path.join(videos_root_val, 'annotations_val.txt')
 
 dataset_val = VideoFrameDataset(
         root_path=videos_root_val,
         annotationfile_path=annotation_file_val,
-        num_segments=6,
+        num_segments=num_seg,
         frames_per_segment=1,
         imagefile_template='img_{:05d}.jpg',
         transform=preprocess,
@@ -70,23 +101,24 @@ dataset_val = VideoFrameDataset(
     )
 val_dataloader = torch.utils.data.DataLoader(
         dataset=dataset_val,
-        batch_size=2,
+        batch_size=batch,
         shuffle=True,
-        num_workers=0,
+        num_workers=4,
         pin_memory=True
     )
 #val_dataloader = dataloader
 
 
 model = make_kinetics_mvit()
+#model = make_kinetics_resnet()
 if torch.cuda.is_available():
     model = model.cuda()
-learning_rate = 0.001
-weight_decay = 0.01
+
+
 optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
 scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.1)
-epoch = 10
+epoch = 100
 min_valid_loss = np.inf
 for i in range(epoch):
     training_loss = 0.0
@@ -103,8 +135,8 @@ for i in range(epoch):
         loss.backward()
         optimizer.step()
         training_loss += loss.item()
-          # print every 2000 mini-batches
-    scheduler.step()
+
+    #scheduler.step()
 
     valid_loss = 0.0
     total = 0.0
@@ -124,9 +156,10 @@ for i in range(epoch):
         loss = F.cross_entropy(pre, labels)
         # Calculate Loss
         valid_loss += loss.item()
+        print(f'Accuracy of the network on the validation: {100 * correct / total} %')
 
     print(f'Epoch {i + 1} \t\t Training Loss: { training_loss / len(dataloader)} \t\t Validation Loss: { valid_loss / len(val_dataloader)}')
-    print(f'Accuracy of the network on the validation: {100 * correct // total} %')
+    print(f'Accuracy of the network on the validation: {100 * correct / total} %')
 
     if min_valid_loss > valid_loss:
         print(f'Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f}) \t Saving The Model')
